b6a188e7f2f5cccbd79d4aedfe544266016f084c
Emerson <emersonbnp@gmail.com>
2017-12-17 07:12:01 -0300

    init

---
+++ src/main/java/ponto_eletronico/ponto_eletronico/App.java
@@ -0,0 +1,13 @@
+package ponto_eletronico.ponto_eletronico;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
